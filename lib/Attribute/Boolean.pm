1e3e2d61b70db6a7521bac5c288b43ac9e768dde
CliffS <cliff@may.be>
2014-04-13 15:26:59 +0100

    Almost finished.

--- lib/Attribute/Boolean.pm
+++ lib/Attribute/Boolean.pm
@@ -1,52 +1,116 @@
 package Attribute::Boolean;
 
-use 5.14.0;
 use strict;
 use warnings FATAL => 'all';
+use 5.14.0;
+use utf8;
 
 =head1 NAME
 
-Attribute::Boolean - The great new Attribute::Boolean!
+Attribute::Boolean - Mark scalars as pure booleans
+
+=cut
+
+use Attribute::Handlers;
+use Variable::Magic qw( wizard cast );
+use Carp;
+use parent 'Exporter';
+
+use Attribute::Boolean::Value;
 
 =head1 VERSION
 
-Version 0.01
+Version v0.0.1;
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = v0.0.1;
 
+=head1 SYNOPSYS
 
-=head1 SYNOPSIS
+This allows you to flag a variable as a boolean.
+In numeric context, it will have the value 0 or 1.
+In string context is will have the falue "false" or "true".
+In JSON, it will correctly return false or true values.
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Attribute::Boolean;
-
-    my $foo = Attribute::Boolean->new();
-    ...
+    my $bool : Boolean;
+    print $bool;    # "false"
+    $bool = (1 + 2 == 3);
+    print $bool;    # "true"
+    print $bool ? "yes" : "no";	 # "yes"
+    $bool = false;
+    print $bool ? "yes" : "no";	 # "no"
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
+This exports constants true and false.
 
 =cut
 
-sub function1 {
+our @EXPORT = qw{ true false };
+
+sub import {
+# add this package to callers @ISA, as attributes only work via inheritance
+    my $class = shift;
+    my $caller = caller;
+    {
+	no strict 'refs';
+	push @{ "${ caller }::ISA" }, __PACKAGE__;
+    }
+    $class->export_to_level(1, $class, @_);
 }
 
-=head2 function2
+=head1 USAGE
 
+An attribute can be declared boolean as follows:
+
+    my $bool : Boolean;
+or
+    my $bool : Boolean = true;
+
+If any perl true value is assigned, the variable is true, if any
+perl false value assigned, the variable is false.
+
+=head2 true
+
+This returns 1 in numeric context, "true" in string context.
+
+=head2 false
+
+This returns 0 in numeric context, "false" in string context.
+
+=head2 TO_JSON
+
+Provided that allow_blessed is set on the JSON (or JSON::XS) object,
+the variable will correctly convert to JSON true or false.
+
+    my $json = new JSON;
+    $json->pretty->convert_blessed;
+    my $bool : Boolean;
+    my %hash = (
+	value => $bool,
+	me    => true,
+    );
+    print $json->encode(\%hash);    # {
+				    #     "value" : false,
+				    #     "me"    : true
+				    # }
+				    
 =cut
 
-sub function2 {
+sub Boolean : ATTR(SCALAR)
+{
+    my ($class, $symbol, $ref, $name, undef, $phase) = @_;
+    cast $$ref, wizard(
+	'set' => sub {
+	    my $ref = shift;
+	    $$ref = $$ref ? true : false;
+	},
+	'get' => sub {
+	    my $ref = shift;
+	    $$ref = $$ref ? true : false;
+	},
+    );
 }
 
 =head1 AUTHOR
@@ -68,74 +132,20 @@ You can find documentation for this module with the perldoc command.
 
     perldoc Attribute::Boolean
 
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=attribute-boolean+>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/attribute-boolean+>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/attribute-boolean+>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/attribute-boolean+/>
-
-=back
-
-
 =head1 ACKNOWLEDGEMENTS
 
+Alan Haggai Alavi C<< <alanhaggai@alanhaggai.org> >> for his
+Scalar::Boolean module L<Scalar::Boolean> which was the inspiration
+for this module.
 
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2014 Cliff Stanford.
 
 This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+under the same terms as Perl itself.
 
 =cut
 
 1; # End of Attribute::Boolean
+
